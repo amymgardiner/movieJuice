dca697ddf65f8fb71e8794e97e7466fdfbb1109e
Amy Gardiner <amymgardiner@gmail.com>
2022-06-17 15:44:53 -0500

    pseudocode for js

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+// global vars
+
+// update page to the default clearing out any dynamic children
+
+// fetch data from the apis then call a function with that data inside to update the elements
+
+// new york times api
+// Site https://developer.nytimes.com/docs/movie-reviews-api/1/overview
+// EXAMPLE https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=yourkey
+
+// imdb api
+// this one is where will get all the data for actors/directors/genre/title ect...
+// Site https://imdb-api.com/
+// https://imdb-api.com/en/API/SearchMovie/k_12345678/inception%202010
+
+// it also has an api for rotten tomatoes reviews
+https://imdb-api.com/en/API/MetacriticReviews/k_12345678/tt1375666
+
+// Update dynamic elements using the data from the fetch
+
+// search click handler after click it does the fetch
+
+// event listener for search button that takes in searchClickHandler() function
