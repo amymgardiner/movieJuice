60db35988853d55d6840b27c8f5867d00035dd2c
Amy Gardiner <amymgardiner@gmail.com>
2022-06-18 15:23:32 -0500

    got all of imdb api to load onto page, starting ny api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
 // global vars
 var mainElement = document.getElementById('main-container');
 var searchBtn = document.getElementById('button-addon3')
-
+var mainContent = ''
 var imdbKey = "k_hgh27181"
 var nyKey = "qahEBcpxGK8ZuOKPZA5GjnMtifJClbCm"
 
@@ -13,7 +13,6 @@ function clearScreen () {
 // fetch data from the apis then call a function with that data inside to update the elements
 function fetchMovieId (movieName) {
     var imdbUrl = `https://imdb-api.com/en/API/SearchMovie/${imdbKey}/${movieName}`
-    var nyUrl = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${movieName}&api-key=${nyKey}`
 
     fetch(imdbUrl)
     .then(function(response){
@@ -21,17 +20,74 @@ function fetchMovieId (movieName) {
             response.json().then(function(data){
                 var movieId = data.results[0].id
                 console.log(movieId)
-                fetchMovieInfo(movieId)
-
+                fetchMovieInfo(movieId, movieName)
+                fetchReview(movieId)
             })
         }
     })
 
 }
 
+function fetchReview (movieId) {
+    var imdbUrl = `https://imdb-api.com/en/API/Ratings/${imdbKey}/${movieId}`
 
-function fetchMovieInfo (movieId) {
+    fetch(imdbUrl)
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                var imdbRating = data.imDb
+                var rottenTomatoesRating = data.rottenTomatoes
+                var metacriticRating = data.metacritic
+                var moviedbRating = data.theMovieDb
+                var filmAffinityRating = data.filmAffinity
+
+                console.log(data)
+
+                mainContent += `<div class="review w-full flex flex-wrap items-center justify-around">
+                <div class="review-card flex">
+                    <h2>IMDb</h2>
+                    <p>${imdbRating}/10</p>
+                </div>
+                <div class="review-card flex">
+                    <h2>Rotten Tomatoes</h2>
+                    <p>${rottenTomatoesRating}%</p>
+                </div>
+                <div class="review-card flex">
+                    <h2>Metacritic</h2>
+                    <p>${metacriticRating}%</p>
+                </div>
+                <div class="review-card flex">
+                    <h2>The Movie DB</h2>
+                    <p>${moviedbRating}/10</p>
+                </div>
+                <div class="review-card flex">
+                    <h2>Film Affinity</h2>
+                    <p>${filmAffinityRating}/10</p>
+                </div>
+            </div>`
+
+            mainElement.innerHTML = mainContent
+            })
+        }
+    })
+}
+
+
+function fetchMovieInfo (movieId, movieName) {
     var imdbUrl = `https://imdb-api.com/en/API/Title/${imdbKey}/${movieId}`
+    var nyUrl = `https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=${movieName}&api-key=${nyKey}`
+    var nyReview = ""
+
+    fetch(nyUrl)
+    .then(function(response){
+        if(response.ok){
+            response.json().then(function(data){
+                console.log(data)
+                nyReview = data.summary_short
+            })
+        }
+    })
+
 
     fetch(imdbUrl)
     .then(function(response){
@@ -43,6 +99,23 @@ function fetchMovieInfo (movieId) {
                 var director = data.directors
                 var stars = data.stars
                 var year = data.year
+
+                console.log(data)
+                
+                mainContent = ''
+                mainContent = `<div class="movie-card">
+                <div class="movie-title">${title}</div>
+                <div class="content">
+                    <div class="movie-img"><img src="${poster}" alt=""></div>
+                    <div class="movie-info">
+                    <ul>
+                        <li>${year}</li>
+                        <li>${genre}</li>
+                        <li>${director}</li>
+                        <li>${stars}</li>
+                        <li>${nyReview}</li></ul></div>
+                </div>
+            </div>`
             })
         }
     })
