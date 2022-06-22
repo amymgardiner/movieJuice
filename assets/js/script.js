1d938f1aca5ff68cb5893e11370c81ed74475d19
Amy Gardiner <amymgardiner@gmail.com>
2022-06-22 16:38:30 -0500

    new imdb api key

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var mainElement = document.getElementById("main-container");
 var searchBtn = document.getElementById("button-addon3");
 var inputElement = document.getElementById("search-input");
 var mainContent = "";
-var imdbKey = "k_4old2p2l";
+var imdbKey = "k_hgh27181";
 var giphyKey = "nxvenzenl4mKAFjuGsTFLvNKZsecjEcP";
 var giphySrc;
 var isLoadingApi = false;
